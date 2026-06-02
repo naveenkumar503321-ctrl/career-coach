2318bd90cb3f3d4d27aa55841322b1e75c764b47
BABBU <keesarinaveenkumar@keesaris-MacBook-Air.local>
2026-06-02 20:45:38 +0530

    first commit

---
+++ app/(main)/onboarding/page.jsx
@@ -0,0 +1,19 @@
+import { industries } from '@/data/industries'
+import OnboardingForm from './_components/onboarding-form'
+import { getUserOnboardingStatus } from '@/actions/user'
+import { redirect } from "next/navigation";
+const OnboardingPage = async() => {
+
+  //check if user is already onboarded
+  const {isOnboarded}=await getUserOnboardingStatus()
+  if(isOnboarded){
+    redirect("/dashboard")
+  }
+  return (
+    <main>
+      <OnboardingForm industries={industries}/>
+    </main>
+  )
+}
+
+export default OnboardingPage
